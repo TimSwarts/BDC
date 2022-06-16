d62ed3ccd13b0142dd47158f3bb82ae1b1ee2a48
tswarts <t.swarts@st.hanze.nl>
2022-06-16 16:22:50 +0200

    update

--- Assignment2/assignment2.py
+++ Assignment2/assignment2.py
@@ -1,5 +1,6 @@
 import multiprocessing as mp
 from multiprocessing.managers import BaseManager, SyncManager
+import argparse as ap
 import os, sys, time, queue
 
 
@@ -8,6 +9,25 @@ ERROR = "DOH"
 IP = ''
 PORTNUM = 5381
 AUTHKEY = b'whathasitgotinitspocketsesss?'
+
+def argument_parser():
+    argparser = ap.ArgumentParser(description="Script voor Opdracht 2 van Big Data Computing;  Calculate PHRED scores over the network.")
+    mode = argparser.add_mutually_exclusive_group(required=True)
+    mode.add_argument("-s", action="store_true", help="Run the program in Server mode; see extra options needed below")
+    mode.add_argument("-c", action="store_true", help="Run the program in Client mode; see extra options needed below")
+    server_args = argparser.add_argument_group(title="Arguments when run in server mode")
+    server_args.add_argument("-o", action="store", dest="csvfile", type=ap.FileType('w', encoding='UTF-8'),
+                required=False, help="CSV file om de output in op te slaan. Default is output naar terminal STDOUT")
+    server_args.add_argument("fastq_files", action="store", type=ap.FileType('r'), nargs='*', help="Minstens 1 Illumina Fastq Format file om te verwerken")
+    server_args.add_argument("--chunks", action="store", type=int, required=True)
+
+    client_args = argparser.add_argument_group(title="Arguments when run in client mode")
+    client_args.add_argument("-n", action="store",
+                dest="n", required=False, type=int,
+                help="Aantal cores om te gebruiken per host.")
+    client_args.add_argument("--host", action="store", type=str, help="The hostname where the Server is listening")
+    client_args.add_argument("--port", action="store", type=int, help="The port on which the Server is listening")
+
 
 
 def make_server_manager(port, authkey):
