f3db42a297d56a238f830e1e42817e8ebdd26757
tswarts <t.swarts@st.hanze.nl>
2024-06-06 14:09:13 +0200

    Renamed timing scripts and updated their settings

--- Assignment6/mpi_files/mpi_timer.sh
+++ Assignment6/mpi_files/mpi_timer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=mpi_timings
-#SBATCH --output=./output/mpi_timings.out
-#SBATCH --error=./output/mpi_timings.err
+#SBATCH --output=./output/slurm_logs/mpi_timer_%j.out
+#SBATCH --error=./output/slurm_logs/mpi_timer_%j.err
 #SBATCH --time=0-08:00:00
 #SBATCH --partition=assemblix
 #SBATCH --nodelist=assemblix2019
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=15
 
 
 #  Make an output directory for the accuracy results
@@ -19,14 +19,16 @@ echo "Ranks,Time" > ./output/mpi_timings.csv
 source /commons/conda/conda_load.sh;
 
 # Loop through the number of tasks
-for ranks in {1..10}; do
+for ranks in {1..64}; do
     if [ $ranks -eq 1 ]; then
         echo "$ranks,NULL" >> ./output/mpi_timings.csv
         continue
     fi
+    run_start_time=$(date +"%d-%m-%y %T")
+    echo "Started running with $ranks ranks at $run_start_time"
     # Run the Python script with mpiexec, using -np to specify the number of ranks
     start_time=$(date +%s.%N)
-    mpiexec -np $ranks python3 ./mpi_files/assignment6_mpi.py -n 10 -s 8000 -b 3000 -e 25
+    mpiexec -np $ranks python3 ./mpi_files/assignment6_mpi.py -n 15 -s 10000 -b 8000 -e 30
     end_time=$(date +%s.%N)
 
     # Calculate the elapsed time in seconds
--- Assignment6/multiprocessing_files/pool_map_timer.sh
+++ Assignment6/multiprocessing_files/pool_map_timer.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 #SBATCH --job-name=pool_map_timings
-#SBATCH --output=./output/pool_map_timings.out
-#SBATCH --error=./output/pool_map_timings.err
+#SBATCH --output=./output/slurm_logs/pool_map_time_%j.out
+#SBATCH --error=./output/slurm_logs/pool_map_time_%j.err
 #SBATCH --time=0-08:00:00
 #SBATCH --partition=assemblix
 #SBATCH --nodelist=assemblix2019
 #SBATCH --nodes=1
-#SBATCH --cpus-per-task=10
+#SBATCH --cpus-per-task=15
 
 
 # Create the output directory if it doesn't exist
@@ -17,9 +17,11 @@ echo "Cores,Time" > ./output/pool_timings.csv
 
 # Loop through the number of cores from 1 to 10
 for cores in {1..10}; do
+    run_start_time=$(date +"%d-%m-%y %T")
+    echo "Started running with $cores ranks at $run_start_time"
     # Run the Python script with the specified number of cores and time the execution
     start_time=$(date +%s.%N)
-    python3 ./multiprocessing_files/assignment6.py -n 10 -c $cores -s 8000 -b 3000 -e 25 # eventually: -n8 -s 10000 -b 6000 -e 25
+    python3 ./multiprocessing_files/assignment6.py -n 15 -c $cores -s 10000 -b 8000 -e 30 # eventually: -n8 -s 10000 -b 6000 -e 25
     end_time=$(date +%s.%N)
 
     # Calculate the elapsed time in seconds
